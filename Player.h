def102997797e833c8fffe29492324097b2d8199
Dhruvch1244 <dr3515@srmist.edu.in>
2023-01-01 22:24:45 +0530

    Changes to be committed: 	modified:   ../Main.cpp 	new file:   ../Map 	modified:   ../Player.cpp 	modified:   ../Player.h 	new file:   ../Wall

---
+++ Player.h
@@ -0,0 +1,34 @@
+#include<SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include <SDL_thread.h>
+#include <iostream>
+#include<cmath>
+class Player
+{
+private: 
+	SDL_Rect cropRect;
+	SDL_Texture *texture;
+	float moveSpeed;
+	float frameCounter; 
+	int frameWidth, frameHeight;
+	int textureWidth;
+	bool isActive; 
+	SDL_Scancode keys[4];
+	int originX, originY;
+	int radius;
+public:
+	Player(SDL_Renderer *renderTarget, std::string filePath, int x, int y, int framesX, int framesY);
+	~Player();
+
+	void Update(float delta, const Uint8 *keyState); 
+	void Draw(SDL_Renderer *renderTarget, SDL_Rect cameraRect); 
+	bool IntersectsWith(Player &p);
+
+	SDL_Rect positionRect;
+	
+	int GetOriginX();
+	int GetOriginY();
+	int GetRadius();
+};
